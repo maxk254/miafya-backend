d560e0a0132f404644e0b7820de4ff09aa8d300b
max254 <maxkamau77@gmail.com>
2025-09-04 21:10:52 +0300

    miafya-backend

---
+++ Server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 5000
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api end point
+app.get('/',(req,res)=>{
+  res.send("API WORKING")
+})
+
+app.listen(port,()=>console.log("server started on port", port))
