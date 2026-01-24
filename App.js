f9fdb07e9b7333a4931f51f46ed2988bf77fd06e
max254 <maxkamau77@gmail.com>
2026-01-24 17:45:36 +0300

    added cookiesParser

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import passport, { initialize } from "passport";
+import passport from "passport";
+import authRoutes from "./src/routes/v1/authRoutes";
+import passport, { initializePassport } from "passport";
 import dotenv  from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(cookieParser());
 app.use(passport.initialize());
 initializePassport(passport);
 
 
+app.use("/auth", authRoutes);
 
 app.get("/", async (req, res)=>{
-  //
+  // for demo to be set up on middlewares
   const userId = (req, res).userId;
   if (!userId) return res.status(401).json({Error: "Not uthenticated"})
     res.json({userId});
