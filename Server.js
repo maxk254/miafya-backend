ad09208afe605ee287d32d39b6d4a009bd403028
max254 <maxkamau77@gmail.com>
2026-01-24 17:47:06 +0300

    imported cors

--- Server.js
+++ Server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
+import dotenv  from 'dotenv'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import adminRouter from './routes/adminRoute.js'
+
+// load env vars first
+dotenv.config();
+
 // app config
 const app = express()
 const port = process.env.PORT || 5000
