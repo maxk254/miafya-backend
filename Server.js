6c7a513f2874b10b97d52a0debd0a8ae280ea0fa
max254 <maxkamau77@gmail.com>
2026-01-26 00:02:10 +0300

    added admin api

--- Server.js
+++ Server.js
@@ -1,36 +1,48 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv  from 'dotenv'
-import connectDB from './config/mongodb.js'
+import connectDB from './config/db.js'
 import connectCloudinary from './config/cloudinary.js'
-import userRouter from './routes/userRoute.js'
-import adminRouter from './routes/adminRoute.js'
+import cookieParser from 'cookie-parser'
 
-// load env vars first
-dotenv.config();
-
+import authRoutes from './routes/v1/authRoutes.js';
+import adminRouters from './routes/v1/adminRoutes.js'
+import facilityRoutes from './routes/v1/facilityRoutes.js'
+import professionalRoutes from './routes/v1/professionalRoutes.js'
+import userRouters from './routes/v1/userRoutes.js'
 // app config
+dotenv.config();
+connectDB();
+connectCloudinary();
+
 const app = express()
 const port = process.env.PORT || 5000
-connectDB()
-connectCloudinary()
+
 
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json()) // Reads JSON body
+app.use(cookieParser()); // reads cookies
+app.use(cors({
+  origin: '' , // frontend url
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 
-// api end point
-app.use('/api/admin',adminRouter);
-// localhost: 5000/api/admin
+// Routes
+app.use('/api/auth',authRoutes); //  auth api
 
-app.use('/api/user',userRouter);
-//localhost:500/api/register
+app.use('/api/user', userRouters); // user api
 
-app.use("/api/user", userRouter);
-//localhost:5000/api/login
+app.use("/api/professional", professionalRoutes); // professional api
 
+app.use('/api/facility', facilityRoutes); // facility api
+
+app.use('/api/admin', adminRouters); // admin api
+
+// Base Route
 app.get('/',(req,res)=>{
   res.send("API WORKING")
 })
 
+// start server
 app.listen(port,()=>console.log("server started on port", port))
