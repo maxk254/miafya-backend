ca2874426cc6cccbd3f1b52612a6408e4fcd405a
max254 <maxkamau77@gmail.com>
2026-01-28 01:00:47 +0300

    crated a function for facilities to be able to update profile

--- controllers/facilityController.js
+++ controllers/facilityController.js
@@ -1,1 +1,37 @@
- // Get facility , filter services offered by different facilities
+// facilities to update their profiles
+import FacilityProfile from "../models/FacilityProfile";
+
+// To get facility profile from the data base
+export const getProfile = async (req, res) =>{
+  try {
+  const profile = await FacilityProfile.findById({user: req.user.id})
+    .populate('user', 'name email phone isVerified');
+  
+    if (!profile) return res.status(400).json({error: "facility not found"});
+    res.json(profile);
+  } catch (error) {
+    res.status(500).json({error: "server Error"})
+  }
+};
+
+// updates facilitry details
+export const updateProfile = async (req, res) => {
+  try{
+    const updates = req.body;
+
+    // potected fields
+    delete updates.user;
+    delete updates.isverified;
+    delete updates.facilityRegistrationNumber;
+
+    const profile = await FacilityProfile.findOneAndUpdate(
+      {user: req.user.id},
+      {$set: updates, location},
+      {new: true, runValidators: true}
+    );
+
+    res.json({message: "facility updated", profile});
+  } catch (error) {
+    res.status(500).json({error: "Server Error"})
+  }
+};
