fdde22672e46bcb11b1f514de5d25e04cb4e649c
max254 <maxkamau77@gmail.com>
2026-01-28 01:01:41 +0300

    fixed spelling error

--- controllers/professionalControler.js
+++ controllers/professionalControler.js
@@ -1,1 +1,39 @@
-// API for healthprofesionals to create their accounts to join our platform
+// API for healthprofesionals to be able to update their profiles and set their profile properly
+
+import professionalProfile from '../models/professionalProfile.js';
+
+
+// Get Professional profile
+export const getProfile = async (req, res) => {
+  try {
+    const profile = await professionalProfile.findById({user: req.user.id})
+      .populate("user", "name email phone is Verified");
+
+      if (!profile) return res.status(400).json({error: "profile not found"});
+      res.json(profile);
+  } catch (error) {
+    res.status(500).json({error: "Server Error"});
+  }
+};
+
+// update Healthprofessional profile details
+export const updateProfile = async (req, res) =>{
+  try {
+    const updates = req.body;
+
+    // protected routes cannot change this information
+    delete updates.user;
+    delete updates.licenseNumber;
+    delete updates.isVerified; // only admin can change this after verification
+  
+    const profile = await professionalProfile.findOneAndUpdate(
+      {user: req.user.id},
+      {$set: updates},
+      {new: true, runValidators: true}
+    );
+
+    res.json({message: "Proile updated", profile});
+  } catch (error) {
+    res.status(500).json({error: "server Erro"})
+  }
+}
